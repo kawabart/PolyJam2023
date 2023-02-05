3383fde25d47923dc5939893ae5673e51f5e03da
xDayton <adamdox@gmail.com>
2023-02-05 14:46:05 +0100

    Add point counting

--- Rootman/Assets/Scripts/Shooting.cs
+++ Rootman/Assets/Scripts/Shooting.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public GameObject cameraObject;
+    public int killPoints = 10;
     public float shootingTimeSeconds = 0.3f;
     private float lastShootTimeSeconds = 0;
     private PlayerInputs playerInputs;
+    private CountPoints countPoints;
 
     // Start is called before the first frame update
     void Start()
     {
         playerInputs = GetComponent<PlayerInputs>();
+        countPoints = GetComponent<CountPoints>();
     }
 
     // Update is called once per frame
@@ -26,6 +29,7 @@ public class Shooting : MonoBehaviour
             Debug.Log(layerMask.value.ToString());
             if (Physics.Raycast(transform.position, cameraObject.transform.forward, out hit, float.MaxValue, layerMask.value))
             {
+                countPoints.AddPoints(killPoints);
                 Destroy(hit.transform.gameObject);
             }
         }
